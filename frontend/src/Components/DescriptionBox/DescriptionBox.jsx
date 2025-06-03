79ce317ccc8b5823712b3a87c3caec7c81d242b3
HARIKRISHNA95504 <patimaharikrishnap@gmail.com>
2025-06-03 17:24:21 +0530

    cartitems page

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './DescriptionBox.css'
+const DescriptionBox =()=>{
+    return(
+        <div className="descriptionbox">
+            <div className="descriptionbox-navigator">
+                <div className="description-nav-box">Description</div>
+                <div className="description-nav-box fade">Reviews (122)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>Application:The Faucet Replacement Part set is perfect to replace the old kitchen sink faucets, bathroom taps and lavatory faucets spout head, offering you clean, clear water to drink, suitable for homes, hotels, public facilities and other places, which is conducive to water saving and filtering water</p>
+                <p>Application:The Faucet Replacement Part set is perfect to replace the old kitchen sink faucets, bathroom taps and lavatory faucets spout head, offering you clean, clear water to drink, suitable for homes, hotels, public facilities and other places, which is conducive to water saving and filtering water</p>
+
+            </div>
+        </div>
+    )
+}
+export default DescriptionBox;

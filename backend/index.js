f63b7c8eacd5b8ee612b9d15d57d1558a1191c6f
HARIKRISHNA95504 <patimaharikrishnap@gmail.com>
2025-06-19 16:28:55 +0530

    created login and signup endpoints

--- backend/index.js
+++ backend/index.js
@@ -128,6 +128,77 @@ app.post('/upload',upload.single('product'),(req,res)=>{
     response.send(products);
  })
 
+ // Schema creating for User MOdel
+ const Users = mongoose.model('Users',{
+    name:{
+        type:String,
+    },
+    email:{
+        type:String,
+        unique:true
+    },
+    password:{
+        type:String,
+    },
+    cartData:{
+        type:Object,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    }
+ })
+
+ // Creating End Point For registering the user
+ app.post('/signup',async(request,response)=>{
+        let check = await Users.findOne({email:request.body.email})
+        if(check){
+          return  response.status(400).json({success:false,errors:"existing user found with same email or mobile"})
+        }
+        let cart ={};
+        for(let i = 0;i<300;i++){
+            cart[i]=0;
+        }
+        const user = new Users({
+            name:request.body.username,
+            email:request.body.email,
+            password:request.body.password,
+            cartData:cart,
+        })
+        await user.save();
+        const data = {
+            user:{
+                id:user.id
+            }
+        }
+        const token = jwt.sign(data,'secret_ecom');
+        response.json({success:true,token})
+ })
+
+ // Creating end point to the User Login
+  app.post('/login',async(request,response)=>{
+    let user = await Users.findOne({email:request.body.email});
+    if(user){
+        const passCompare = request.body.password === user.password;
+        if(passCompare){
+            const data = {
+                user:{
+                    id:user.id
+                }
+            }
+            const token = jwt.sign(data,'secret_ecom');
+            response.json({success:true,token});
+        }
+        else{
+            response.json({
+                success:false,errors:"wrong Password"
+            })
+        }
+    }else{
+        response.json({success:false,errors:"Wrong Email Id"})
+    }
+  })
+
 app.listen(port,(error)=>{
     if(!error){
         console.log('Server Running on Port' +port)
